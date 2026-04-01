ec969c9f98116b2ad362109b81d6fe95443b3cf6
sahujeevika1101-maker <sahujeevika1101@gmail.com>
2026-04-01 14:18:13 +0530

    updated

--- UserAccount/app/models.py
+++ UserAccount/app/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Item(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    
 
     def __str__(self):
         return self.title
